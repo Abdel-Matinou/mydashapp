b1835373c9009bd4394e3b00375e432c386d82d6
sarah_abdel <abdel-matinou.akalah@atexo.com>
2020-04-25 21:32:08 +0000

    Adding navbar, title and cards

--- dash_app.py
+++ dash_app.py
@@ -1,49 +1,111 @@
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df_tickets = pd.read_csv('/Users/fabriceakalah/Desktop/demandes.csv', encoding = 'macgreek', sep=';')
+tickets_place = df_tickets[df_tickets['Produit']=='PLACE']
+tickets_mpe = df_tickets[(df_tickets['Produit'] != 'MAXIMILIEN') & (df_tickets['Produit'] != 'PLACE') & (df_tickets['Produit'] != 'TerNum')]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
+##############################################################################################
+# APP BODY #
 
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
+## NAVBAR
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("A propos", href="#")),
+    ],
+    brand="UTAH ++",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
 
-    dcc.Graph(
-        id='example-graph-2',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
+##NAVBAR END
+
+welcome = html.H2('Bienvenue sur le dashbord d\'utah', style =
+                    {
+                    "textAlign": 'center',
+                    "margin" : '30px',
+                    'textTransform' : 'uppercase'
+                    }
+                )
+
+###### CSV UPLOAD
+
+###### CSV UPLOAD END
+
+######---FIRST ROW OF INFORMATION ---
+
+first_card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H5("Tickets Total", className="card-title"),
+            html.H4(f"{df_tickets.shape[0]}"),
+        ]
     )
+)
+
+second_card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H5("Tickets Place", className="card-title"),
+            html.H4(f"{tickets_place.shape[0]}"),
+        ]
+    )
+)
+
+third_card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H5("Tickets MPE", className="card-title"),
+            html.H4(f"{tickets_mpe.shape[0]}"),
+        ]
+    )
+)
+
+
+# time_card = dbc.Card(
+#     dbc.CardBody(
+#         [
+#             html.H5("Etendue temporel", className="card-title"),
+#             html.P(
+#                 f"Le plus ancien ticket date du : {df_tickets['Date de création'].min()} "
+#             ),
+#             html.P(
+#                 f"Le plus récent ticket date du : {df_tickets['Date de création'].max()}"
+#             ),
+#         ]
+#     )
+# )
+
+
+cards = dbc.Row([dbc.Col(first_card), dbc.Col(second_card), dbc.Col(third_card)])
+
+
+######---FIRST ROW END
+
+app.layout = dbc.Container(children = [
+                    html.Div(navbar), welcome, cards,
+
+
+    # dbc.Row([
+    #     html.H2(f"Total tickets : {df.shape[0]-1}"),
+    #
+    #     html.H2(f"Total agents : {len(df['Agent support en charge'].unique())}"),
+    # ])
+
 ])
 
 
 
+
+##############################
+# run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
